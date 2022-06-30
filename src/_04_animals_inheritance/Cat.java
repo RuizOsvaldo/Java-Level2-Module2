58eae65d89847de024cacbbcc6a7dbdad560de9a
Osvaldo <osvaldo.ruiz@jointheleague.org>
2022-06-29 19:59:26 -0700

    Finished first inheritance recipe

--- src/_04_animals_inheritance/Cat.java
+++ src/_04_animals_inheritance/Cat.java
@@ -1,5 +1,7 @@
 package _04_animals_inheritance;
 
-public class Cat {
-
+public class Cat extends Animal {
+	Cat(String name, String furColor, boolean isGirl){
+		super(name,furColor, isGirl);
+	}
 }
